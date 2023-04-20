604c9baf05a07ab6281edcccbf1326dbf8062a92
Yusuf <ysfayds41@gmail.com>
2023-04-20 19:04:58 +0300

    finish sidebar

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import twitterLogo from "../images/twitter.svg";
 import {
   BookmarksIcon,
   TweetMoreIcon,
@@ -25,24 +26,80 @@ import {
   ShareIcon,
   SearchIcon,
 } from "../icons/Icons";
+import SideLink from "../components/SideLink";
+import UserBox from "../components/UserBox";
+
+const sideLinks = [
+  {
+    name: "Home",
+    icon: HomeIcon,
+  },
+  {
+    name: "Explore",
+    icon: ExploreIcon,
+  },
+  {
+    name: "Notifications",
+    icon: NotificationsIcon,
+  },
+
+  {
+    name: "Messsages",
+    icon: MessagesIcon,
+  },
+  {
+    name: "Bookmarks",
+    icon: BookmarksIcon,
+  },
+  {
+    name: "Lists",
+    icon: ListsIcon,
+  },
+  {
+    name: "Profile",
+    icon: ProfileIcon,
+  },
+  {
+    name: "More",
+    icon: MoreIcon,
+  },
+];
 
 const Sidebar = () => {
+  const [active, setActive] = useState("Home")
+  const handleMenuItemClick = (name) => {
+    setActive(name)
+  }
   return (
-    <div className="w-72 bg-blue-200">
-      <h1>Sidebar</h1>
-      <HomeIcon />
+    <div className="flex flex-col justify-between w-72  px-2">
+      <div>
+        <div className="mt-1 mb-4 ml-1 flex items-center justify-center w-12 h-12 rounded-full hover:bg-gray-lightest transform transition-colors duration-200">
+          <img src={twitterLogo} alt="Twitter Logo" className="w-9 h-9" />
+        </div>
+        <nav className="mb-4">
+          <ul>
+            {sideLinks.map(({name,icon}) => (
+              <SideLink key={name} name={name} Icon={icon} active={active} onMenuItemClick={handleMenuItemClick}/>
+            )
+
+            )}
+          </ul>
+        </nav>
+        <button className="bg-primary-base hover:bg-primary-dark text-white shadow-lg rounded-full py-3 px-8 w-11/12 transform transition-colors duration-200">
+          Tweet
+        </button>
+      </div>
+      <UserBox/>
+      {/* <HomeIcon />
       <ExploreIcon />
       <NotificationsIcon />
       <MessagesIcon />
       <BookmarksIcon />
       <ListsIcon />
       <ProfileIcon />
-      <MoreIcon />
-      <hr />
-      <hr />
-      <hr />
+      <MoreIcon /> */}
 
-      <DirectionIcon />
+      {/* <DirectionIcon />
       <EarthIcon />
       <ImageIcon />
       <GIFIcon />
@@ -50,10 +107,6 @@ const Sidebar = () => {
       <EmojiIcon />
       <ScheduleIcon />
       <LocationIcon />
-
-      <hr />
-      <hr />
-      <hr />
 
       <VerifiedIcon />
       <TweetMoreIcon />
@@ -63,11 +116,9 @@ const Sidebar = () => {
       <ViewIcon />
       <ShareIcon />
 
-      <hr />
-      <hr />
-      <hr />
+      
 
-      <SearchIcon />
+      <SearchIcon /> */}
     </div>
   );
 };
